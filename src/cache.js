ca8fe81aff33e62414750fd7f462be72d5b3450c
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2023-09-16 10:28:24 +0300

    Added implementation for basic tests

--- src/cache.js
+++ src/cache.js
@@ -1,4 +1,28 @@
-class Cache{
+class Cache {
+    constructor() {
+        this._dat = new Map();
+        // return new Proxy(this, {
+        //     // get: (obj, key) => {
+        //     //     const v = obj._dat.get(key);
+        //     //     if (v === undefined)
+        //     //         return null;
+        //     //     else
+        //     //         return v;
+        //     // },
+        //     set: (obj, key, value) => { return obj._dat.set(key, value) }
+        // })
+    }
 
+    set(key, value, n_queries = 1) {
+        return this._dat.set(key, value);
+    }
+
+    get(key) {
+        const v = this._dat.get(key);
+        if (v === undefined)
+            return null;
+        else
+            return v;
+    }
 }
-export {Cache}
+export { Cache }
