f6f69b109d1a546b05d8eef8107a7a53265175a4
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2023-09-16 20:24:22 +0300

    Added tests for n_queries

--- test/cache.test.js
+++ test/cache.test.js
@@ -15,3 +15,28 @@ describe('basic set/get tests', () => {
         expect(c.get(k)).toBeNull();
     });
 });
+
+describe('queries functionality tests', () => {
+    const k = 'test';
+    const k2 = 'jest';
+    const v = 1;
+
+    it('shouldn\'t return value if n_queries reaches zero', () => {
+        const c = new Cache();
+        c.set(k, v);
+        expect(c.get(k)).toEqual(v);
+        expect(c.get(k)).toBeNull();
+    })
+
+    it('should respect specified n_queries', () => {
+        const c = new Cache();
+        c.set(k, v, 0);
+        expect(c.get(k)).toBeNull();
+
+        c.set(k2, v, 10);
+        for (let i = 0; i < 10; i++) {
+            expect(c.get(k2)).toEqual(v);
+        }
+        expect(c.get(k2)).toBeNull();
+    });
+});
