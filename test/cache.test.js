817e701f4a597bd532571c3b81fa876025d85cf6
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2023-09-16 10:27:43 +0300

    Added basic set/get tests

--- test/cache.test.js
+++ test/cache.test.js
@@ -1,5 +1,17 @@
-import {Cache} from "../src/cache";
+import { Cache } from "../src/cache";
 
-test('it fails', () => {
-    expect(false).toBe(true);
+describe('basic set/get tests', () => {
+    const k = 'test';
+    const v = 1;
+
+    it('should return data that was set', () => {
+        const c = new Cache();
+        c.set(k, v);
+        expect(c.get(k)).toBe(v);
+    });
+
+    it('should return null if data that wasn\'t set', () => {
+        const c = new Cache();
+        expect(c.get(k)).toBeNull();
+    });
 });
