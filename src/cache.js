084a971046c5f75c54d89d10ddfd0e6998014826
COOLIRON2311 <ignatenko.ivan.23112000@gmail.com>
2023-09-16 22:24:00 +0300

    Added implementation for stats

--- src/cache.js
+++ src/cache.js
@@ -1,33 +1,34 @@
 class Cache {
     #dat;
 
-    constructor() {
+    constructor(options = null) {
         this.#dat = new Map();
-        // return new Proxy(this, {
-        //     // get: (obj, key) => {
-        //     //     const v = obj._dat.get(key);
-        //     //     if (v === undefined)
-        //     //         return null;
-        //     //     else
-        //     //         return v;
-        //     // },
-        //     set: (obj, key, value) => { return obj._dat.set(key, value) }
-        // })
+        options?.forEach(element => {
+            this.set(...element);
+        });
     }
 
     set(key, value, n_queries = 1) {
-        this.#dat.set(key, {value: value, queries: n_queries});
+        this.#dat.set(key, { value: value, queries: n_queries });
     }
 
     get(key) {
         const v = this.#dat.get(key);
         if (v === undefined || v.queries === 0)
             return null;
-        else
-        {
+        else {
             v.queries--;
             return v.value;
         }
+    }
+
+    stats() {
+        // return Array.from(this.#dat.entries()).map(([k, v]) => ({
+        return [...this.#dat.entries()].map(([k, v]) => ({
+            key: k,
+            value: v.value,
+            queries: v.queries
+        }));
     }
 }
 export { Cache }
